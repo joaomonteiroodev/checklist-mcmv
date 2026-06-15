fc07bc43f980c013b15accfaa3b2d128b4c1cc48
joaomonteiroodev <joao.monteirocorp@gmail.com>
2026-06-15 08:00:14 -0300

    Protegendo credenciais do Firebase com variáveis de ambiente

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRX-PoPWj5nRLw9kOaKfIMhy5mtL9aDTg",
-  authDomain: "checklist-mcmv.firebaseapp.com",
-  projectId: "checklist-mcmv",
-  storageBucket: "checklist-mcmv.firebasestorage.app",
-  messagingSenderId: "452869588978",
-  appId: "1:452869588978:web:d9f5609ef3faef98d38d6b",
-  measurementId: "G-8BYS0R3JSG",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
